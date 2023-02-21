01530eb7231ebe19dac083760eecae20534bd211
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-02-21 20:42:24 -0300

    mejoras

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
-const estilos = {
-  color: 'yellow',
-}
 
 function App() {
+
   return (
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style ={estilos}>
-          Bienvenidos a mi aplicacion
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          En construccion
-        </a>
-      </header>
+    <div>
+      <h2>
+        EN CONSTRUCCION
+      </h2>
+
     </div>
   );
 }
 
-export default App;
