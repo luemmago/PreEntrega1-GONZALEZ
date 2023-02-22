b7488b67e654583978f12d7628230963d72373e6
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-02-22 00:55:52 -0300

    BOTON

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,15 @@
+import './ItemListContainer.css';
+
+const ItemListContainer =() => {
+     return (
+
+        <div className="contenedor">
+            <h2> LISTADO DE PRODUCTOS </h2>
+            <hr />
+
+
+        </div>
+    )
+}    
+
+export default ItemListContainer
