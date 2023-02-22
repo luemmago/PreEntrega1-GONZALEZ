06f5096282c402349ab9c6e7ada184044a11e166
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-02-21 23:57:31 -0300

    iconocs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar.js";
 import CartWidget from "./components/CartWidget.js";
+import './App.css'
 
 function App() {
 
   return (
     <div>
       <Navbar />
-     
-      <img src="https://www.limitedeportes.com/wp-content/uploads/2022/06/1046-1.jpg" width= '200' height='200' border='10'/>
+      <br></br>
+      <img src="https://www.limitedeportes.com/wp-content/uploads/2022/06/1046-1.jpg" className="san"/>
       <p>Camiseta de San Martin de Tucuman</p>
     
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,51 +6,44 @@ const Navbar = () => {
 
       <h1> FUTBOL PASION </h1>
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Inicio</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Inicio</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Seleccion Argenina</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="https://www.adidas.com.ar/argentina">Seleccion Argenina</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Primera Division</a>
+              <li className="nav-item">
+                <a className="nav-link" href="https://www.adidas.com.ar/river_plate">Primera Division</a>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
                   aria-expanded="false">
                   Otras Categorias
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Primera Nacional</a></li>
-                  <li><a class="dropdown-item" href="#">Divisiones menores</a></li>
+                  <li><a className="dropdown-item" href="#">Primera Nacional</a></li>
+                  <li><a className="dropdown-item" href="#">Divisiones menores</a></li>
                   <li>
-                    <hr class="dropdown-divider">
+                    <hr className="dropdown-divider"/>
                   </li>
-                  <li><a class="dropdown-item" href="#">Algo más aquí</a></li>
+                  <li><a className="dropdown-item" href="#">Algo más aquí</a></li>
                 </ul>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
-                <button class="btn btn-outline-success" type="submit">Buscar</button>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar"/>
+                <button className="btn btn-outline-success" type="submit">Buscar</button>
             </form>
           </div>
         </div>
       </nav>
-
-      <h2>
-        <a href="https://www.adidas.com.ar/argentina"><il>Seleccion Argentina</il></a>
-        <br />
-        <a href="https://www.adidas.com.ar/river_plate"><il>Primera Division </il></a>
-      </h2>
-
-
+      
     </div>
   )
 }
