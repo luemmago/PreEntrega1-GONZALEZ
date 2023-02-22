ab5eb0ba7e844dfb4524d786e32559664eb226d1
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-02-21 23:28:38 -0300

    iconos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ function App() {
   return (
     <div>
       <Navbar />
+     
+      <img src="https://www.limitedeportes.com/wp-content/uploads/2022/06/1046-1.jpg" width= '200' height='200' border='10'/>
+      <p>Camiseta de San Martin de Tucuman</p>
+    
+
+
+
     </div>
+
+  
+
   );
 }
 
