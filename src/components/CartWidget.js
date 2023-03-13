f5f4cb3296fbea891d49f93c092dff6400086af0
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2023-03-12 21:50:59 -0300

    cartw

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,12 +1,13 @@
+import Navbar from "./navbar"
+
 export const CartWidget = () => {
 
     return(
 
        <div>
-      
+            <img src="/FUTBOL-LIVE/iconos/TNS.png" alt> </img>
        </div>
 
     )
 }
-
 export default CartWidget
